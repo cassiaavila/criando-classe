c77b2b983ef9b4534f09cd7d950e42dc10e7d818
Aparecida de Cassia <sdcassia2@gmail.com>
2024-10-03 21:25:47 -0300

    Criação da classe vehicle

---
+++ vehicle.ts
@@ -0,0 +1,29 @@
+class Vehicle {
+    private ano: string;
+    private modelo: string;
+    private placa: string;
+    private cor: string;
+    constructor(ano: string, modelo: string, placa: string, cor: string) {
+        this.ano=ano;
+        this.modelo =modelo;
+        this.placa =placa;
+        this.cor = cor;
+    }
+
+    //toString é usado para descrever a classe
+    public toString (): string{
+        return `Carro Ano ${this.ano}, Modelo ${this.modelo}, Placa ${this.placa}, Cor ${this.cor}`;
+    }
+    public print (){
+        console.log (`Carro Ano ${this.ano}, Modelo ${this.modelo}, Placa ${this.placa}, Cor ${this.cor}`)
+    }
+
+
+}
+
+function main(){
+    const car = new Vehicle("2013", "Corsa", "olx-1997", "Azul" )
+    car.print()
+    //console.log(car.toString())
+}
+main()
